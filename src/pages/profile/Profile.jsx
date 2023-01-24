969562da7b39bf6179ec3765d4d7fa5d848461dc
Shawn Kebel <fxrender@outlook.com>
2023-01-23 21:34:50 -0600

    :construction: Work on `Profile page`

---
+++ src/pages/profile/Profile.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import Feed from "../../components/feed/Feed";
+import Leftbar from "../../components/leftbar/Leftbar";
+import Rightbar from "../../components/rightbar/Rightbar";
+import Topbar from "../../components/topbar/Topbar";
+import "./profile.css";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+
+export default function Profile() {
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const [user, setUser] = useState({});
+  const username = useParams().username;
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const res = await axios.get(`/users?username=${username}`);
+      setUser(res.data);
+    };
+    fetchUser();
+  }, [username]);
+
+  return (
+    <>
+      <Topbar />
+      <div className="profile">
+        <Leftbar />
+        <div className="profileRight">
+          <div className="profileRightTop">
+            <div className="profileCover">
+              <img src={user.coverPicture || PF + "person/noCover.png"} alt="" className="profileCoverImg" />
+              <img src={user.profilePicture || PF + "person/noAvatar.png"} alt="" className="profileUserImg" />
+            </div>
+            <div className="profileInfo">
+              <h4 className="profileInfoName">{user.username}</h4>
+              <span className="profileInfoDesc">{user.desc}</span>
+            </div>
+          </div>
+          <div className="profileRightBottom">
+            <Feed username={username} />
+            <Rightbar user={user} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
