ee7a824ff489030cea574b3cb40388a372f76a2b
Shawn Kebel <fxrender@outlook.com>
2023-01-23 20:46:16 -0600

    :boom: Add `Topbar`

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./topbar.css";
+import { Chat, Notifications, Person, Search } from "@material-ui/icons";
+import { Link } from "react-router-dom";
+
+export default function Topbar() {
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span className="logo"> S͛OͦCͨIͥAͣLiͥzeͤ</span>
+        </Link>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <Search className="searchIcon" />
+          <input placeholder="Search for friends, posts, videos..." className="searchInput" />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink">Home</span>
+          <span className="topbarLink">Timeline</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <Person />
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <div className="topbarIconItem">
+            <Chat />
+            <span className="topbarIconBadge">3</span>
+          </div>
+          <div className="topbarIconItem">
+            <Notifications />
+            <span className="topbarIconBadge">2</span>
+          </div>
+        </div>
+        <img src="/assets/person/person040.png" alt="" className="topbarImg" />
+      </div>
+    </div>
+  );
+}
