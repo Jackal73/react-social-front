b8fbec49e8bc2cffd0a867f60f9bfbe7b2ff54dc
Shawn Kebel <fxrender@outlook.com>
2023-01-23 21:00:10 -0600

    :construction: Start `rightbar` component - home page

---
+++ src/components/rightbar/Rightbar.jsx
@@ -0,0 +1,25 @@
+import "./rightbar.css";
+import { Users } from "../../dummyData";
+import Online from "../online/Online";
+
+export default function Rightbar({ user }) {
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const HomeRightbar = () => {
+    return (
+      <>
+        <div className="birthdayContainer">
+          <img src="assets/gift.png" alt="" className="birthdayImg" />
+          <span className="birthdayText">
+            <b>Paula Foster</b> and <b>3 other friends</b> have birthdays today
+          </span>
+        </div>
+        <img src="assets/ad.png" alt="" className="rightbarAd" />
+        <h4 className="rightbarTitle">Friends Online</h4>
+        <ul className="rightbarFriendList">
+          {Users.map((u) => (
+            <Online key={u.id} user={u} />
+          ))}
+        </ul>
+      </>
+    );
+  };
