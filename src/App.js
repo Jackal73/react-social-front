82c3dc0fe19c0995b3897625124f08d6b7a04803
Shawn Kebel <fxrender@outlook.com>
2023-01-23 21:36:28 -0600

    :necktie: Add `Profile` link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
@@ -16,9 +17,9 @@ function App() {
         <Route path="/register">
           <Register />
         </Route>
-        >
-          Learn React
-        </a>
+        <Route path="/profile/:username">
+          <Profile />
+        </Route>
       </Switch>
     </Router>
   );
