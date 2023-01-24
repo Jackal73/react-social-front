ac349b15d04aa0bd4bf9a8d2422d3e74cc540a35
Shawn Kebel <fxrender@outlook.com>
2023-01-23 21:35:49 -0600

    :boom: Add `Profile` variant to `Rightbar`

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -23,3 +23,78 @@ export default function Rightbar({ user }) {
       </>
     );
   };
+
+  const ProfileRightbar = () => {
+    const onClick = () => {
+      console.log("edit button clicked");
+    };
+    return (
+      <>
+        <div className="rightbarProfile">
+          <button className="editBtn" onClick={onClick}>
+            Edit
+          </button>
+          <h4 className="rightbarTitlePro">Your Profile</h4>
+
+          <div className="rightbarInfo">
+            <div className="rightbarInfoItem">
+              <span className="rightbarInfoKey">Located:</span>
+              <span className="rightbarInfoValue">{user.city}</span>
+            </div>
+            <div className="rightbarInfoItem">
+              <span className="rightbarInfoKey">From:</span>
+              <span className="rightbarInfoValue">{user.from}</span>
+            </div>
+            <div className="rightbarInfoItem">
+              <span className="rightbarInfoKey">Work:</span>
+              <span className="rightbarInfoValue">{user.job}</span>
+            </div>
+            <div className="rightbarInfoItem">
+              <span className="rightbarInfoKey">Relationship:</span>
+              <span className="rightbarInfoValue">
+                {user.relationship === 1 ? "Single" : user.relationship === 2 ? "Married" : "-"}
+              </span>
+            </div>
+            <div className="rightbarInfoItem">
+              <span className="rightbarInfoKey">Website:</span>
+              <span className="rightbarInfoValue">kblDesigners.com</span>
+            </div>
+          </div>
+        </div>
+        <h4 className="rightbarTitle">Your friends</h4>
+        <div className="rightbarFollowings">
+          <div className="rightbarFollowing">
+            <img src={`${PF}person/1.jpeg`} alt="" className="rightbarFollowingImg" />
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img src={`${PF}person/2.jpeg`} alt="" className="rightbarFollowingImg" />
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img src={`${PF}person/3.jpeg`} alt="" className="rightbarFollowingImg" />
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img src={`${PF}person/4.jpeg`} alt="" className="rightbarFollowingImg" />
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img src={`${PF}person/5.jpeg`} alt="" className="rightbarFollowingImg" />
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img src={`${PF}person/6.jpeg`} alt="" className="rightbarFollowingImg" />
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+        </div>
+      </>
+    );
+  };
+
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">{user ? <ProfileRightbar /> : <HomeRightbar />}</div>
+    </div>
+  );
+}
